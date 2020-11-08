9557e46c9c919f7c1dec8ce8a32b0e1a79a97141
Narendar Singh Shekhawat <narendarshekhawat4@gmail.com>
2020-11-08 16:12:12 +0530

    commit by Narendar

--- e-mobileconnectmodel/src/main/java/com/app/springboot/emobileconnect/service/EMConnectionService.java
+++ e-mobileconnectmodel/src/main/java/com/app/springboot/emobileconnect/service/EMConnectionService.java
@@ -2,11 +2,9 @@ package com.app.springboot.emobileconnect.service;
 
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.XSlf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-@XSlf4j
 @RequiredArgsConstructor
 public class EMConnectionService {
 
